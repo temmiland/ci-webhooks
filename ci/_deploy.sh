daa2b2794ecd4e15ed672fbaf4814273b2401fb5
Temmi Pietsch <welcome@temmi.land>
2025-09-20 11:02:39 +0200

    d

--- ci/_deploy.sh
+++ ci/_deploy.sh
@@ -11,21 +11,19 @@ fi
 LOG_FILE="/opt/webhook/logs/deploy_$REPO_NAME.log"
 STATUS_FILE="/opt/webhook/status/deploy_$REPO_NAME.json"
 
-REPO=$(/usr/bin/jq -c --arg name "$REPO_NAME" '.repos[] | select(.name==$name)' /opt/webhook/repos.json || true)
-echo $REPO
+REPO=$(jq -c --arg name "$REPO_NAME" '.repos[] | select(.name==$name)' /opt/webhook/repos.json || true)
+
 if [ -z "$REPO" ]; then
     echo '{"status":"error","message":"Repo $REPO_NAME not found in repos.json"}' > "$STATUS_FILE"
     exit 1
 fi
 
-PATH=$(echo "$REPO" | /usr/bin/jq -r '.path')
-BRANCH=$(echo "$REPO" | /usr/bin/jq -r '.branch')
-SCRIPT=$(echo "$REPO" | /usr/bin/jq -r '.deploy_script')
-echo $PATH
-echo $BRANCH
-echo $SCRIPT
-if [ ! -d "$PATH" ]; then
-    echo '{"status":"error","message":"Repo path $PATH does not exist"}' > "$STATUS_FILE"
+REPO_PATH=$(echo "$REPO" | jq -r '.path')
+BRANCH=$(echo "$REPO" | jq -r '.branch')
+SCRIPT=$(echo "$REPO" | jq -r '.deploy_script')
+
+if [ ! -d "$REPO_PATH" ]; then
+    echo '{"status":"error","message":"Repo path $REPO_PATH does not exist"}' > "$STATUS_FILE"
     exit 1
 fi
 
